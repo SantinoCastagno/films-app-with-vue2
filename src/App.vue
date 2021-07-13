a02896fc1f3fe81fad74772c13de517cd41dd9dd
Santino Castagno <santinocastagno.dev@gmail.com>
2021-07-13 01:16:37 -0300

    First commit to Vue project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  /*
+  Colors palette
+  kBlackColor:  #171717
+  kGreyColor:   #444444
+  kRedColor:    #DA0037
+  kWhiteColor:  #EDEDED
+  */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
