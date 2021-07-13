00cc512fd0faf35ccbd9e07b968c6b212a9ac265
Santino Castagno <santinocastagno.dev@gmail.com>
2021-07-13 20:56:19 -0300

    First steps

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
+
 <template>
   <div id="app">
+    <div class=title >
+      <h1>Films app with Vue 2</h1>
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,18 +13,35 @@
 </template>
 
 <style>
-#app {
-  /*
+/*
   Colors palette
   kBlackColor:  #171717
   kGreyColor:   #444444
   kRedColor:    #DA0037
   kWhiteColor:  #EDEDED
-  */
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+*/
+@import url('https://fonts.googleapis.com/css2?family=Zen+Loop&display=swap');
+
+
+
+
+body {
+  overflow-x: hidden;
+  overflow-y: hidden;
+
+  font-family: 'Zen Loop', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  width: 100vw;
+  height: 100vh;
+  background: #444444;
+  color: #EDEDED;
+
+  font-size: 3rem;
+}
+
+#app {
   text-align: center;
-  color: #2c3e50;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <h1>What are you looking for?</h1>
+  </div>
 </template>
 
 <script>
